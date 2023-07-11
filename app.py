defd4069d9d7d9dce0409c9673b76596a10f31a7
oneuzbekcoder <131743366+oneuzbekcoder@users.noreply.github.com>
2023-07-11 23:18:55 +0500

    34

--- app.py
+++ app.py
@@ -12,22 +12,34 @@ logging.basicConfig(level=logging.DEBUG)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+def webAppKeyboard(): #создание клавиатуры с webapp кнопкой
+   keyboard = types.ReplyKeyboardMarkup(row_width=1) #создаем клавиатуру
+   webAppTest = WebAppInfo(url="https://oneuzbekcoder.github.io") #создаем webappinfo - формат хранения url
+   one_butt = types.KeyboardButton(text="Тестовая страница", web_app=webAppTest) #создаем кнопку типа webapp
+   keyboard.add(one_butt) #добавляем кнопки в клавиатуру
+
+   return keyboard #возвращаем клавиатуру
+
+
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     markup = types.ReplyKeyboardMarkup()
     markup.add(types.KeyboardButton("Sahifani ochish",web_app=WebAppInfo(url='https://oneuzbekcoder.github.io')))
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.",reply_markup=markup)
+    # bot.send_message( message.chat.id, 'Привет, я бот для проверки телеграмм webapps!)', reply_markup=webAppKeyboard()) 
+
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.",reply_markup=webAppKeyboard())
 
 # @dp.message_handler(content_types=['web_app_data'])
 # async def progress(message: types.Message):
 #     print(message)
 #     await message.answer(f"hello {message.web_app_data.data}" )
 
-@bot.message_handler(content_types="web_app_data") #получаем отправленные данные 
+@dp.message_handler(content_types="web_app_data") #получаем отправленные данные 
 def answer(webAppMes):
    print(webAppMes) #вся информация о сообщении
    print(webAppMes.web_app_data.data) #конкретно то что мы передали в бота
-   bot.send_message(webAppMes.chat.id, f"получили инофрмацию из веб-приложения: {webAppMes.web_app_data.data}") 
+   dp.send_message(webAppMes.chat.id, f"получили инофрмацию из веб-приложения: {webAppMes.web_app_data.data}") 
    #отправляем сообщение в ответ на отправку данных из веб-приложения 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
