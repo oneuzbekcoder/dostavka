f69e0dc6bf4f2a37e7790f0e4faff8cd00f138c0
oneuzbekcoder <xalqoboditpark@gmail.com>
2024-07-05 07:10:06 +0500

    1

---
+++ script.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+
+    // let tg = window.Telegram.WebApp;
+	// tg.expand();
+	// tg.MainButton.textColor = '#FFFFFF';
+	// tg.MainButton.color = '#2cab37';
+
+	// let item = "";
+
+	// let btn1 = document.getElementById("btn1");
+
+	// btn1.addEventListener("click", function(){
+	// 	if (tg.MainButton.isVisible) {
+	// 		tg.MainButton.hide();
+	// 	}
+	// 	else {
+	// 		tg.MainButton.setText("Вы выбрали товар 1!");
+	// 		item = "1";
+	// 		tg.MainButton.show();
+	// 	}
+	// });
+
+	// Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	// 	tg.sendData(item);
+	// });
+
+
+	// let usercard = document.getElementById("usercard");
+
+	// let p = document.createElement("p");
+
+	// p.innerText = `${tg.initDataUnsafe.user.first_name}
+	// ${tg.initDataUnsafe.user.last_name}`;
+
+	// usercard.appendChild(p);
+});
